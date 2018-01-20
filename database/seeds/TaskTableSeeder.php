460ed7b33f13e36b27ab5febdf76292abcc42080
marcpre <marcus.presich@gmail.com>
2018-01-20 08:51:45 +0100

    adding faker & creating TaskTableSeeder

---
+++ database/seeds/TaskTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $limit = 345;
+        
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('tasks')->insert([
+                'description' => $faker->sentence(),
+                'archive' => $faker->boolean($chanceOfGettingTrue = 50),
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::get('current_tasks', 'TaskController@index');
+Route::get('archived_tasks', 'TaskController@archived');
+Route::post('create_task', 'TaskController@store');
+Route::post('delete_task/{id}', 'TaskController@destroy');
+Route::post('archive_task/{id}', 'TaskController@archive');
+Route::post('edit_task', 'TaskController@edit');
